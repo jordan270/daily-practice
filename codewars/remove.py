72a20a76432f69858de3cad86f82031c7e186145
Garv <garvkapoor834@gmail.com>
2026-07-21 17:17:34 +0530

    Add remove_smallest solution

---
+++ codewars/remove.py
@@ -0,0 +1,8 @@
+def remove_smallest(numbers):
+    if not numbers:
+        return []
+    
+    result = numbers.copy()          
+    min_index = result.index(min(result))  
+    result.pop(min_index)
+    return result
